9b3ecd672971faf59891f833fe417531dd93c1cd
zhoupipipipipipi <601139730@qq.com>
2018-01-18 10:42:30 +0800

    2017作品集

---
+++ public/public/javascripts/hoverDemo.js
@@ -0,0 +1,18 @@
+$('.item-btn').click(function(){
+	var i = 0;
+	var e = $(this);
+	show(e);
+});
+
+function show(e){
+	$('.item-hover').hide();
+	var id = e.attr('data-id');
+	$('.item-hover'+id).show();
+	if(!e.hasClass('item-btn-active')){
+		$('.item-btn-active').removeClass('item-btn-active');
+		e.addClass('item-btn-active');
+	}
+}
+
+show($('.item-btn-active'));
+
